fd2580afe816fa3736df11b449e3d8cd1e5ad1c1
owwix <53359125+owwix@users.noreply.github.com>
2026-02-12 09:54:42 -0800

    no more hitting broken api paths

--- web/src/lib/payloadSdk/payloadUrl.ts
+++ web/src/lib/payloadSdk/payloadUrl.ts
@@ -1,11 +1,11 @@
 export const getPayloadBaseUrl = () => {
   if (typeof window !== "undefined") {
-    return "";
+    return process.env.NEXT_PUBLIC_PAYLOAD_URL ?? "";
   }
   return (
     process.env.PAYLOAD_URL ??
-    process.env.NEXT_PUBLIC_SITE_URL ??
     process.env.NEXT_PUBLIC_PAYLOAD_URL ??
+    process.env.NEXT_PUBLIC_SITE_URL ??
     "http://localhost:3000"
   );
 };
